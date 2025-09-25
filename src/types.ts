ecb69d6507ca0298acd216d019c80e65436b4cd8
Andrei-17 <apst.andrei@gmail.com>
2025-09-25 12:45:36 +0200

    fix spacing

--- src/types.ts
+++ src/types.ts
@@ -2,18 +2,18 @@ import { Viewport, ScreenshotOptions, PDFOptions } from "puppeteer";
 import { Data } from "ejs";
 
 export interface ImageRenderConfig {
-  viewport?: Viewport;
-  options: ScreenshotOptions;
-  data?: Data;
-  delayMs?: number;
-  metadata?: ImageMetadata;
+    viewport?: Viewport;
+    options: ScreenshotOptions;
+    data?: Data;
+    delayMs?: number;
+    metadata?: ImageMetadata;
 }
 
 export interface PDFRenderConfig {
-  options: PDFOptions;
-  data?: Data;
-  delayMs?: number;
-  metadata?: PDFMetadata;
+    options: PDFOptions;
+    data?: Data;
+    delayMs?: number;
+    metadata?: PDFMetadata;
 }
 
 export interface ImageMetadata {
@@ -21,14 +21,14 @@ export interface ImageMetadata {
 }
 
 export interface PDFMetadata {
-  title?: string;
-  author?: string;
-  subject?: string;
-  keywords?: string[];
-  creator?: string;
-  producer?: string;
-  creationDate?: Date;
-  modDate?: Date;
-  language?: string;
-  custom?: Record<string, string | number | boolean>;
+    title?: string;
+    author?: string;
+    subject?: string;
+    keywords?: string[];
+    creator?: string;
+    producer?: string;
+    creationDate?: Date;
+    modDate?: Date;
+    language?: string;
+    custom?: Record<string, string | number | boolean>;
 }
